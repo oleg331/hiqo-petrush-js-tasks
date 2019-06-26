e28142f32bf09e498e2253dbbcf8d8c4dca25bff
Oleg Petrush <oleg.petrush@gmail.com>
2019-06-26 18:48:12 +0300

    Add solution task3

---
+++ 26.06/task3.js
@@ -0,0 +1,18 @@
+/*
+	Task 3
+*/
+
+function isValueExists(value) {
+  return value === null ? 
+    false : !isNaN(value) ?
+    !isNaN(value) : false;
+}
+
+// Expected result
+isValueExists(1); // => true
+isValueExists(0); // => true
+isValueExists('12'); // => true
+isValueExists(''); // => true
+isValueExists(false); // => true
+isValueExists(undefined); // => false
+isValueExists(null); // => false
