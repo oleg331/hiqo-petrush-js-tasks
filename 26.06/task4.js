6533c54bf39b292dcabdbf387a3085652ed4777a
Oleg Petrush <oleg.petrush@gmail.com>
2019-06-26 18:48:17 +0300

    Add solution task4

---
+++ 26.06/task4.js
@@ -0,0 +1,18 @@
+/*
+	Task 4
+*/
+
+function getProduct(funct1, funct2) {
+  return doubleValue(getFour());
+}
+
+// Expected result
+getProduct(doubleValue, getFour);
+
+function doubleValue(value) {
+  return value * 2;
+}
+
+function getFour() {
+  return 4;
+}
